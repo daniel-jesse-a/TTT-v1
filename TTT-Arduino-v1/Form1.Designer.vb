6b9af269f05fa2cc71f4c434b5842b56eebf05b3
hiswo <hiswo@DANIELJ>
2022-08-20 12:12:36 +0530

    Add project files.

---
+++ TTT-Arduino-v1/Form1.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'SerialPort1
+        '
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(47, 66)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 100)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(153, 66)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 100)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Location = New System.Drawing.Point(259, 66)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 100)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Location = New System.Drawing.Point(47, 172)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 100)
+        Me.Button4.TabIndex = 3
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Location = New System.Drawing.Point(153, 172)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(100, 100)
+        Me.Button5.TabIndex = 4
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Location = New System.Drawing.Point(259, 172)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(100, 100)
+        Me.Button6.TabIndex = 5
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button7.Location = New System.Drawing.Point(47, 278)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(100, 100)
+        Me.Button7.TabIndex = 6
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button8.Location = New System.Drawing.Point(153, 278)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(100, 100)
+        Me.Button8.TabIndex = 7
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button9.Location = New System.Drawing.Point(259, 278)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(100, 100)
+        Me.Button9.TabIndex = 8
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(47, 418)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(143, 40)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "Play Again"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(216, 418)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(143, 40)
+        Me.Button11.TabIndex = 10
+        Me.Button11.Text = "Reset"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(47, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(259, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(153, 25)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Label3"
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(47, 480)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(143, 23)
+        Me.Button12.TabIndex = 14
+        Me.Button12.Text = "Connect"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(388, 66)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(110, 312)
+        Me.RichTextBox1.TabIndex = 15
+        Me.RichTextBox1.Text = ""
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(388, 385)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 13)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "Status:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(510, 526)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Tic Tac Toe"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button12 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ TTT-Arduino-v1/Form1.vb
@@ -0,0 +1,393 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Globalization
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Drawing
+
+Public Class Form1
+
+    Dim running As Boolean = False
+    Dim startTime As DateTime
+    Dim counter As Integer = 0
+
+    Dim winner As String = 20
+    Dim loser As String = 30
+    Dim play As String = 40
+
+    Dim buttons As Button() = {Button1, Button2, Button3, Button4, Button5, Button6, Button7, Button8, Button9}
+    Dim buttoncheck As Button()
+
+    Dim _client As TcpClient
+    Public Function isWinner(ByVal key As Char)
+        Return ((Button1.Text = Button2.Text And Button2.Text = Button3.Text And Button3.Text = key) Or
+            (Button4.Text = Button5.Text And Button5.Text = Button6.Text And Button6.Text = key) Or
+            (Button7.Text = Button8.Text And Button8.Text = Button9.Text And Button9.Text = key) Or
+            (Button1.Text = Button5.Text And Button5.Text = Button9.Text And Button9.Text = key) Or
+            (Button3.Text = Button5.Text And Button5.Text = Button7.Text And Button7.Text = key) Or
+            (Button1.Text = Button4.Text And Button4.Text = Button7.Text And Button7.Text = key) Or
+            (Button2.Text = Button5.Text And Button5.Text = Button8.Text And Button8.Text = key) Or
+            (Button3.Text = Button6.Text And Button6.Text = Button9.Text And Button9.Text = key))
+        Timer1.Stop()
+
+    End Function
+
+
+    Public Function ComputerInput()
+        Dim btns As Button() = {Button1, Button2, Button3, Button4, Button5, Button6, Button7, Button8, Button9}
+        Dim possInput As List(Of Button) = New List(Of Button)
+
+        Dim ns As NetworkStream = _client.GetStream()
+        Dim comp As String
+
+        For j As Integer = 0 To btns.Length - 1
+            If (btns(j).Text = "") Then
+                possInput.Add(btns(j))
+            End If
+        Next
+
+        Dim chs() As Char = {"X", "O"}
+
+        For Each ch As Char In chs
+            For Each btn As Button In possInput
+                btn.Text = ch
+                If isWinner(ch) Then
+                    btn.Text = "X"
+                    comp = btn.Name.Substring(6)
+                    Label3.Text = comp
+                    ns.Write(Encoding.ASCII.GetBytes(comp), 0, comp.Length)
+                    btn.Enabled = False
+                    Return (True)
+                End If
+                btn.Text = ""
+            Next
+        Next
+
+        Dim cornerBtn As List(Of Button) = New List(Of Button)
+
+        For Each btn As Button In possInput
+            If (btn Is Button1 Or btn Is Button3 Or btn Is Button7 Or btn Is Button9) Then
+                cornerBtn.Add(btn)
+            End If
+        Next
+
+        Dim tempBtn As Button
+        If cornerBtn.Count > 0 Then
+            tempBtn = cornerBtn(New Random().Next(0, cornerBtn.Count - 1))
+            tempBtn.Text = "X"
+            comp = tempBtn.Name.Substring(6)
+            Label3.Text = comp
+            ns.Write(Encoding.ASCII.GetBytes(comp), 0, comp.Length)
+            tempBtn.Enabled = False
+            Return (True)
+        End If
+
+        If possInput.Contains(Button5) Then
+            Button5.Text = "X"
+            Button5.Enabled = False
+            Return (True)
+        End If
+
+        Dim edgeBtn As List(Of Button) = New List(Of Button)
+        For Each btn As Button In possInput
+            If (btn Is Button2 Or btn Is Button4 Or btn Is Button6 Or btn Is Button8) Then
+                edgeBtn.Add(btn)
+            End If
+        Next
+
+        If edgeBtn.Count > 0 Then
+            tempBtn = edgeBtn(New Random().Next(0, edgeBtn.Count - 1))
+            tempBtn.Text = "X"
+            tempBtn.Enabled = False
+            Return (True)
+        End If
+
+        Return (False)
+    End Function
+
+    Public Sub reset()
+        Dim btns As Button() = {Button1, Button2, Button3, Button4, Button5, Button6, Button7, Button8, Button9}
+        For Each btn As Button In btns
+            btn.Text = ""
+            btn.Enabled = True
+            btn.BackColor = DefaultBackColor
+
+        Next
+    End Sub
+
+    Public Function checkTheWinner(ByVal key As Char)
+        If (Button1.Text = Button2.Text And Button2.Text = Button3.Text And Button3.Text = key) Then
+            Button1.BackColor = Color.GreenYellow
+            Button2.BackColor = Color.GreenYellow
+            Button3.BackColor = Color.GreenYellow
+        ElseIf (Button4.Text = Button5.Text And Button5.Text = Button6.Text And Button6.Text = key) Then
+            Button4.BackColor = Color.GreenYellow
+            Button5.BackColor = Color.GreenYellow
+            Button6.BackColor = Color.GreenYellow
+        ElseIf (Button7.Text = Button8.Text And Button8.Text = Button9.Text And Button9.Text = key) Then
+            Button7.BackColor = Color.GreenYellow
+            Button8.BackColor = Color.GreenYellow
+            Button9.BackColor = Color.GreenYellow
+        ElseIf (Button1.Text = Button5.Text And Button5.Text = Button9.Text And Button9.Text = key) Then
+            Button1.BackColor = Color.GreenYellow
+            Button5.BackColor = Color.GreenYellow
+            Button9.BackColor = Color.GreenYellow
+        ElseIf (Button3.Text = Button5.Text And Button5.Text = Button7.Text And Button7.Text = key) Then
+            Button3.BackColor = Color.GreenYellow
+            Button5.BackColor = Color.GreenYellow
+            Button7.BackColor = Color.GreenYellow
+        ElseIf (Button1.Text = Button4.Text And Button4.Text = Button7.Text And Button7.Text = key) Then
+            Button1.BackColor = Color.GreenYellow
+            Button4.BackColor = Color.GreenYellow
+            Button7.BackColor = Color.GreenYellow
+        ElseIf (Button2.Text = Button5.Text And Button5.Text = Button8.Text And Button8.Text = key) Then
+            Button2.BackColor = Color.GreenYellow
+            Button5.BackColor = Color.GreenYellow
+            Button8.BackColor = Color.GreenYellow
+        ElseIf (Button3.Text = Button6.Text And Button6.Text = Button9.Text And Button9.Text = key) Then
+            Button3.BackColor = Color.GreenYellow
+            Button6.BackColor = Color.GreenYellow
+            Button9.BackColor = Color.GreenYellow
+        Else
+            Return (False)
+        End If
+        Return (True)
+    End Function
+    Public Sub check()
+        Dim ns As NetworkStream = _client.GetStream()
+        If checkTheWinner("O") Then
+            ns.Write(Encoding.ASCII.GetBytes(winner), 0, winner.Length)
+            Button11.Text = "You Won"
+            Button11.BackColor = Color.GreenYellow
+            Button11.Enabled = False
+            Button10.Enabled = True
+            Timer1.Stop()
+            Timer1.Enabled = False
+        ElseIf checkTheWinner("X") Then
+            ns.Write(Encoding.ASCII.GetBytes(loser), 0, loser.Length)
+            Button11.Text = "You Lose"
+            Button11.BackColor = Color.Red
+            Button11.Enabled = False
+            Button10.Enabled = True
+            Timer1.Stop()
+            Timer1.Enabled = False
+        Else
+            Dim btns As Button() = {Button1, Button2, Button3, Button4, Button5, Button6, Button7, Button8, Button9}
+            Dim possInput As List(Of Button) = New List(Of Button)
+
+            For j As Integer = 0 To btns.Length - 1
+                If (btns(j).Text = "") Then
+                    possInput.Add(btns(j))
+                End If
+            Next
+            If possInput.Count = 0 Then
+                Button11.Text = "Match Tie"
+                Button11.BackColor = Color.Yellow
+                Button11.Enabled = False
+                Button10.Enabled = True
+                Timer1.Stop()
+                Timer1.Enabled = False
+            End If
+
+            For j As Integer = 0 To btns.Length - 1
+                If (btns(j).Text = "") Then
+                    possInput.Add(btns(j))
+                End If
+            Next
+        End If
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.Text = "O"
+        Button1.Enabled = False
+        ComputerInput()
+        check()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Button2.Text = "O"
+        Button2.Enabled = False
+        ComputerInput()
+        check()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Button3.Text = "O"
+        Button3.Enabled = False
+        ComputerInput()
+        check()
+    End Sub
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Button4.Text = "O"
+        Button4.Enabled = False
+        ComputerInput()
+        check()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Button5.Text = "O"
+        Button5.Enabled = False
+        ComputerInput()
+        check()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Button6.Text = "O"
+        Button6.Enabled = False
+        ComputerInput()
+        check()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Button7.Text = "O"
+        Button7.Enabled = False
+        ComputerInput()
+        check()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Button8.Text = "O"
+        Button8.Enabled = False
+        ComputerInput()
+        check()
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Button9.Text = "O"
+        Button9.Enabled = False
+        ComputerInput()
+        check()
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        reset()
+        Button10.Enabled = True
+        Timer1.Stop()
+        counter = 0
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Dim ns As NetworkStream = _client.GetStream()
+        ns.Write(Encoding.ASCII.GetBytes(play), 0, play.Length)
+        reset()
+        Button11.Text = "Reset"
+        Button11.BackColor = DefaultBackColor
+        Button11.Enabled = True
+        Button10.Enabled = False
+        counter = 0
+
+    End Sub
+
+    Private Sub Buttons_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button7.Click, Button8.Click, Button9.Click
+        counter += 1
+
+        If counter < 2 Then
+            startTime = DateTime.Now
+            Timer1.Enabled = True
+            Timer1.Start()
+        End If
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToParent()
+        Windows.Forms.Control.CheckForIllegalCrossThreadCalls = False
+        'Change COM Port Here
+        SerialPort1.PortName = "COM8"
+        SerialPort1.BaudRate = 9600
+        SerialPort1.Open()
+        Timer1.Stop()
+        Label2.Text = "00:00:000"
+
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Dim SrlInp As String = SerialPort1.ReadLine
+
+        counter += 1
+        Label1.Text = counter
+
+        If SrlInp = 1 Then
+            Me.Button1.PerformClick()
+        ElseIf SrlInp = 2 Then
+            Me.Button2.PerformClick()
+        ElseIf SrlInp = 3 Then
+            Me.Button3.PerformClick()
+        ElseIf SrlInp = 4 Then
+            Me.Button4.PerformClick()
+        ElseIf SrlInp = 5 Then
+            Me.Button5.PerformClick()
+        ElseIf SrlInp = 6 Then
+            Me.Button6.PerformClick()
+        ElseIf SrlInp = 7 Then
+            Me.Button7.PerformClick()
+        ElseIf SrlInp = 8 Then
+            Me.Button8.PerformClick()
+        ElseIf SrlInp = 9 Then
+            Me.Button9.PerformClick()
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Dim diff As TimeSpan = DateTime.Now - startTime
+
+        Label2.Text = diff.ToString("mm\:ss\:fff")
+
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Try
+            Dim ip As String = "192.168.5.1"
+            Dim port As Integer = "4096"
+
+            _client = New TcpClient(ip, port)
+
+            CheckForIllegalCrossThreadCalls = False
+
+            Threading.ThreadPool.QueueUserWorkItem(AddressOf ReceiveMessages)
+
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ReceiveMessages(state As Object)
+        Try
+            While True
+
+                Dim ns As NetworkStream = _client.GetStream
+
+                Dim toReceive(100000) As Byte
+                ns.Read(toReceive, 0, toReceive.Length)
+                Dim txt As String = Encoding.ASCII.GetString(toReceive)
+
+                If RichTextBox1.TextLength > 0 Then
+                    RichTextBox1.Text &= vbNewLine & txt
+                Else
+                    RichTextBox1.Text = txt
+                End If
+
+                If txt = 50 Then
+                    Label4.Text = "Status: Connected"
+
+                End If
+
+                If txt = 9 Then
+                    Label4.Text = "Status: Disonnected"
+                End If
+
+            End While
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
